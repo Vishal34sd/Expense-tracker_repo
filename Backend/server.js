14146cb1e5b0ef1a8d3435e4908cc128ac1f84ae
vishal-07 <dubeyvishal714@gmail.com>
2025-07-26 11:38:31 +0530

    fixed finally

--- Backend/server.js
+++ Backend/server.js
@@ -9,14 +9,13 @@ import authRoutes from "./routes/authRoute.js";
 const app = express();
 const PORT = process.env.PORT;
 
-// Connect to DB
 dbConnection();
 
-// Middleware to parse JSON and URL-encoded form data
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// ✅ Allow only your frontend's HTTPS domain
+
 const allowedOrigins = ['https://expense-tracker-repo-3p8w.vercel.app'];
 
 app.use(
@@ -32,14 +31,14 @@ app.use(
   })
 );
 
-// ✅ Handle preflight requests (important!)
+
 app.options('*', cors());
 
-// Routes
+
 app.use("/api/v1", transactionRoutes);
 app.use("/api/v1", authRoutes);
 
-// Start server
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
