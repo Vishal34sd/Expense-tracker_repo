7b51cb8b9e89da0a36ecbe93e862261d336cd77d
Vishal Dubey <dubeyvishal714@gmail.com>
2025-07-30 17:17:28 +0530

    Update vite.config.js

--- Frontend/expense-frontend/vite.config.js
+++ Frontend/expense-frontend/vite.config.js
@@ -1,21 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),  tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://expense-tracker-c5hw.onrender.com',
-        changeOrigin: true,
-        secure: false,
-        
-       // Vite will proxy /api requests to http://localhost:8080/api.
-       //Example - axios.post("/api/v1/login", formData);
-      }
-    }
-  }
-  
-})
+  plugins: [react(), tailwindcss()],
+});
